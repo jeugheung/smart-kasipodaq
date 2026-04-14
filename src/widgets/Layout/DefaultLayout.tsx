4285078f1f9f2370f5a40e9c7e4b791189b81719
Andrey Kim <akart.dev@gmail.com>
2026-04-14 16:11:15 +0500

    first commit

---
+++ src/widgets/Layout/DefaultLayout.tsx
@@ -0,0 +1,4 @@
+import React, { ReactNode } from 'react';
+import { StyleSheet, RefreshControl, View, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
